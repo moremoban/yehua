ea31c3f123c7ce4ff31842d5e6b905eac0ae420c
chfw <wangc_2011@hotmail.com>
2017-02-26 17:40:20 +0000

    code refactoring

--- yehua/main.py
+++ yehua/main.py
@@ -16,56 +16,38 @@ optional = {
 
 padding = " A: "
 
-answers = {
-    'project': 'n/a',
-    'description': 'n/a',
-}
 
 def main():
-    for q in questions.keys():
-        a = raw_input(questions[q] + padding)
-        answers[q] = a
-    
-    if answers['type'] == '2':
-        for q in ['cli']:
-            a = raw_input(optional[q] + padding)
-            answers[q] = a
-    else:
-        a = raw_input(optional['nickname'])
-        answers['nickname'] = a
-    
+    answers = get_user_inputs()
     template_loader = FileSystemLoader(get_resource_dir("templates"))
     jj2_environment = Environment(
         loader=template_loader,
         keep_trailing_newline=True,
         trim_blocks=True,
         lstrip_blocks=True)
-    
+
     project_name = answers['project']
-    project_moban = os.path.join(project_name, '.moban.d')
-    project_moban_tests = os.path.join(project_name, '.moban.d', 'tests')
-    project_tests = os.path.join(project_name, 'tests')
-    docs_path = os.path.join('docs', 'source')
-    project_docs = os.path.join(project_name, 'docs')
-    project_source = os.path.join(project_docs, 'source')
-    project_moban_docs = os.path.join(project_moban, 'docs')
-    project_moban_docs_source = os.path.join(project_moban_docs, 'source')
-    project_src = os.path.join(project_name, project_name.lower().replace('-', '_'))
     os.mkdir(project_name) # project name
-    os.mkdir(project_moban) # project name
-    os.mkdir(project_moban_tests) # project name
-    os.mkdir(project_moban_docs)
-    os.mkdir(project_moban_docs_source)
-    os.mkdir(project_tests) # project name
-    os.mkdir(project_src)
-    os.mkdir(project_docs)
-    os.mkdir(project_source)
-    
+    docs_path = os.path.join('docs', 'source')
+    src_path = project_name.lower().replace('-', '_')
+    directories = [
+        os.path.join('.moban.d'),
+        os.path.join('.moban.d', 'tests'),
+        os.path.join('tests'),
+        os.path.join('docs'),
+        docs_path,
+        os.path.join('.moban.d', 'docs'),
+        os.path.join('.moban.d', 'docs', 'source'),
+        src_path
+    ]
+    for subdir in directories:
+        os.mkdir(os.path.join(project_name, subdir))
+
     template = jj2_environment.get_template('project.yml')
     with open(os.path.join(project_name, project_name + '.yml'), 'w') as f:
         rendered_content = template.render(**answers)
         f.write(rendered_content)
-    
+
     template = jj2_environment.get_template('.moban.yml')
     with open(os.path.join(project_name, '.moban.yml'), 'w') as f:
         rendered_content = template.render(**answers)
@@ -77,25 +59,41 @@ def main():
     for f in template_list:
         if isinstance(f, tuple):
             source = os.path.join(get_resource_dir('templates'), f[0])
-            dest = os.path.join(project_moban, f[1])
+            dest = os.path.join(project_name, '.moban.d', f[1])
         else:
             source = os.path.join(get_resource_dir('templates'), f)
-            dest = os.path.join(project_moban, f)
+            dest = os.path.join(project_name, '.moban.d', f)
         shutil.copy(source, dest)
-    
+
     for f in ['CHANGELOG.rst', 'Makefile']:
         source = os.path.join(get_resource_dir('static'), f)
         dest = os.path.join(project_name, f)
         shutil.copy(source, dest)
-    
+
     for f in [('__init__.py.jj2', '__init__.py')]:
         if isinstance(f, tuple):
             source = os.path.join(get_resource_dir('static'), f[0])
-            dest = os.path.join(project_src, f[1])
+            dest = os.path.join(project_name, src_path, f[1])
         else:
             source = os.path.join(get_resource_dir('static'), f)
-            dest = os.path.join(project_src, f)
+            dest = os.path.join(project_name, src_path, f)
         shutil.copy(source, dest)
+
+
+def get_user_inputs():
+    answers = {}
+    for q in questions.keys():
+        a = raw_input(questions[q] + padding)
+        answers[q] = a
+
+    if answers['type'] == '2':
+        for q in ['cli']:
+            a = raw_input(optional[q] + padding)
+            answers[q] = a
+    else:
+        a = raw_input(optional['nickname'])
+        answers['nickname'] = a
+    return answers
 
 
 def get_resource_dir(folder):
