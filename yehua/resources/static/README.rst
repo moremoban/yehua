25bb1061668022c4cce0fd5ffd5f19d6ea4ca241
chfw <wangc_2011@hotmail.com>
2017-03-18 23:18:10 +0000

    update existing templates

--- yehua/resources/static/README.rst
+++ yehua/resources/static/README.rst
@@ -1,8 +1,33 @@
-{% extends "BASIC-README.rst.jj2" %}
+================================================================================
+{{name}}
+================================================================================
 
-{%block constraint%}
+.. image:: https://api.travis-ci.org/{{organisation}}/{{name}}.svg?branch=master
+   :target: http://travis-ci.org/{{organisation}}/{{name}}
+
+.. image:: https://codecov.io/github/{{organisation}}/{{name}}/coverage.png
+    :target: https://codecov.io/github/{{organisation}}/{{name}}
+
+{%block documentation_link%}
+.. image:: https://readthedocs.org/projects/{{name|lower}}/badge/?version=latest
+   :target: http://{{name|lower}}.readthedocs.org/en/latest/
 {%endblock%}
 
-{%block features %}
-**{{name}}** does xyz.
-{%endblock%}
+
+Installation
+================================================================================
+
+You can install it via pip:
+
+.. code-block:: bash
+
+    $ pip install {{name}}
+
+
+or clone it and install it:
+
+.. code-block:: bash
+
+    $ git clone http://github.com/{{organisation}}/{{name}}.git
+    $ cd {{name}}
+    $ python setup.py install
--- yehua/resources/static/test.sh
+++ yehua/resources/static/test.sh
@@ -1,13 +1,28 @@
-{% extends "test.sh.jj2" %}
+{%block pretest%}
+{%endblock%}
+{%if external_module_library%}
+  {%set package=external_module_library%}
+{%else%}
+  {%if webframework%}
+    {%set package=webframework + '_excel' %}
+  {%else%}
+    {%if misc%}
+      {%set package=misc %}
+    {%else%}
+      {%if command_line_interface%}
+        {%set package=command_line_interface + '_cli' %}
+      {%else%}
+        {% if name == nick_name %}
+          {%set package=name%}
+        {%else%}
+          {%set package='pyexcel_' + nick_name%}
+        {%endif%}
+      {%endif%}
+	{%endif%}
+  {%endif%}
+{%endif%}
+pip freeze
+nosetests --with-cov --cover-package {{package|lower}} --cover-package tests {%if not exclude_doctest%}--with-doctest --doctest-extension=.rst README.rst{%endif%} tests {%if not nodocs%}docs/source{%endif%} {%if not external_module_library%}{{package|lower}}{%endif%} && flake8 . --exclude=.moban.d {%block flake8_options%}--builtins=unicode,xrange,long{%endblock%}
 
-{%block pretest %}
-#/bin/bash
-
-cd tests/test_plugin
-python setup.py install
-cd ../../
-{%endblock %}
-
-{%block flake8_options%}
---builtins=unicode,xrange,long
+{%block posttest%}
 {%endblock%}
