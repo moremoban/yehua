b0a6ee035a7204a01480e3444e748da5ac342243
chfw <wangc_2011@hotmail.com>
2017-03-18 23:31:47 +0000

    update files

--- yehua/resources/static/test.sh
+++ yehua/resources/static/test.sh
@@ -3,22 +3,10 @@
 {%if external_module_library%}
   {%set package=external_module_library%}
 {%else%}
-  {%if webframework%}
-    {%set package=webframework + '_excel' %}
+  {%if command_line_interface%}
+    {%set package=command_line_interface + '_cli' %}
   {%else%}
-    {%if misc%}
-      {%set package=misc %}
-    {%else%}
-      {%if command_line_interface%}
-        {%set package=command_line_interface + '_cli' %}
-      {%else%}
-        {% if name == nick_name %}
-          {%set package=name%}
-        {%else%}
-          {%set package='pyexcel_' + nick_name%}
-        {%endif%}
-      {%endif%}
-	{%endif%}
+    {%set package=name%}
   {%endif%}
 {%endif%}
 pip freeze
