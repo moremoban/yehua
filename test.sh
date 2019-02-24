d29fedf87f92abcf46c33bb8fcb5e43e0561fd31
chfw <wangc_2011@hotmail.com>
2019-02-24 15:46:20 +0000

    :handshake: update test.sh by run moban v0.4 again

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 pip freeze
-nosetests --with-coverage --cover-package yehua --cover-package tests tests  docs/source yehua && flake8 . --exclude=.moban.d,docs --builtins=unicode,xrange,long
+nosetests --with-coverage --cover-package yehua --cover-package tests tests  docs/source yehua
