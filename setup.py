203ebfa611dbe33e7da1a104f59ed52f461bb313
chfw <wangc_2011@hotmail.com>
2020-01-14 23:06:23 +0000

    :hammer: split the lines between pypi-mobans and yehua resources

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
-# Template by pypi-mobans
+"""
+Template by pypi-mobans
+"""
+
+import os
+import sys
 import codecs
 import locale
-import os
 import platform
-import sys
 from shutil import rmtree
 
-from setuptools import Command, find_packages, setup
+from setuptools import Command, setup, find_packages
 
 PY2 = sys.version_info[0] == 2
 PY26 = PY2 and sys.version_info[1] < 7
@@ -58,9 +61,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
-
     "Programming Language :: Python :: 3.7",
-
     "Programming Language :: Python :: 3.8",
 
     'Topic :: Utilities',
@@ -74,7 +75,7 @@ SETUP_COMMANDS = {}
 
 PACKAGES = find_packages(exclude=["ez_setup", "examples", "tests", "tests.*"])
 EXTRAS_REQUIRE = {
-    "pypi-mobans": ['pypi-mobans-pkg==0.0.7'],
+    "pypi-mobans": ['pypi-mobans-pkg==0.0.9'],
     ":python_version == '3.4'": ["ruamel.yaml>=0.15.5,<=0.15.94"],
     ":python_version == '3.7'": ["ruamel.yaml>=0.15.42"],
     ":python_version != '3.4' and python_version < '3.7'": ["ruamel.yaml>=0.15.5"],
